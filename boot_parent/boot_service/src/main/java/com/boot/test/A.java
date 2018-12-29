71059be969c96218b889d1cbb117f8f0bc756f90
chenshunyu <chenshunyu@bestpay.com.cn>
2018-12-29 11:30:30 +0800

    代码回滚

---
+++ boot_parent/boot_service/src/main/java/com/boot/test/A.java
@@ -0,0 +1,13 @@
+package com.boot.test;
+
+/**
+ * @Description: TDD
+ * @Author:chenshunyu
+ * @Date: 2018/12/28 15:01
+ **/
+public class A implements Interf {
+    @Override
+    public void ceLMothod() {
+        System.out.print("我是A");
+    }
+}
---
+++ boot_parent/boot_service/src/main/java/com/boot/test/Interf.java
@@ -0,0 +1,12 @@
+package com.boot.test;
+
+/**
+ * @ClassName:intf
+ * @Description: TDD
+ * @Author:chenshunyu
+ * @Date: 2018/12/28 15:00
+ **/
+public interface Interf {
+
+    void ceLMothod();
+}
