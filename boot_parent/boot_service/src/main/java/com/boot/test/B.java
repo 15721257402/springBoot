047b4a50d9914bc3540d1251c5a09202fcba900d
chenshunyu <chenshunyu@bestpay.com.cn>
2018-12-29 11:08:27 +0800

    第一测提交

---
+++ boot_parent/boot_service/src/main/java/com/boot/test/B.java
@@ -0,0 +1,14 @@
+package com.boot.test;
+
+/**
+ * @Description: TDD
+ * @Author:chenshunyu
+ * @Date: 2018/12/28 15:02
+ **/
+public class B implements Interf {
+    @Override
+    public void ceLMothod() {
+        System.out.print("我是B");
+        System.out.print("第一提交");
+    }
+}
