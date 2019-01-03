286479bf30934eb84c8b2c513dfb0fe35991873a
chenshunyu <chenshunyu@bestpay.com.cn>
2019-01-03 14:52:19 +0800

    第n次提交

--- boot_parent/boot_service/src/main/java/com/boot/TestReset.java
+++ boot_parent/boot_service/src/main/java/com/boot/TestReset.java
@@ -7,9 +7,7 @@ package com.boot;
  **/
 public class TestReset {
     public static void main(String[] args){
-        System.out.print("第一次提交");
-        System.out.print("第二次提交");
-        System.out.print("第三次提交");
+        System.out.print("第n次提交");
         System.out.print("dev提交");
     }
 }
